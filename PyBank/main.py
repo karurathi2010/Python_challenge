60bb5a47e175f7f34f5ca52b12a90a53a2168e65
karurathi2010 <karunakesavan.k@gmail.com>
2024-02-20 14:17:39 -0800

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,45 +10,51 @@ min_value=0
 dates=[]
 
 
+#assigning the  file path of the source data to the variable file_path
 
 file_path=os.path.join('Resources','budget_data.csv')
+
+#open file in 'read' mode .
 with open(file_path) as data:
     lines=csv.reader(data,delimiter=',')
+   
+    # Storing the header row in the variable header
     header=next(lines)
    
     
 
     for row in lines:
 
-        month=row[0].split("-")
-        months.append(month[0])
-        net_amt=net_amt+int(row[1])
-        change.append(int(row[1]))
-        dates.append(row[0])
+        month=row[0].split("-")  #Slit the date column for seperating months form dates.
+        months.append(month[0])     # add all months to the months list.
+        net_amt=net_amt+int(row[1])  # add each row of the Profit/loss column to get the total amount
+        change.append(int(row[1]))   # add the Profit/loss column values to the change list
+        dates.append(row[0])         # add the Date column values to the dates list
+       
        
         
-    for i in range(0,(len(change)-1)):
+    for i in range(0,(len(change)-1)):       #To find the changes in Profit/Losses over the entire period subtracting the adjecent row values in the Profit/Losses column.
         
         x=change[i+1]-change[i]
-        tot_change.append(x)
+        tot_change.append(x)          # store the result in the tot_change list
         
-    grt_value= max(tot_change) 
-    min_value=min(tot_change)  
-    N=tot_change.index(grt_value)  
+    grt_value= max(tot_change)        #Finding the maximum value of the tot_change list to get the greatest increase in profits over the entire period.
+    min_value=min(tot_change)         #Findin the minimum value of the tot_change list to get the greatest decrease in profits over the entire period.
+    N=tot_change.index(grt_value)     # Find the index value of the greatest increase & greatest decrease values for getting corresponding dates.
     M=tot_change.index(min_value)   
         
         
-    for i in range(0,len(tot_change)):
+    for i in range(0,len(tot_change)):   # find the sum of the profit change for calculating the average change.
         add=add+tot_change[i]
 
     
 
-    length=len(tot_change) 
-    avg_chng=round(add/length,2)  
-    Total_months=len(months) 
+    length=len(tot_change)              #Finding the no:of items in the tot_change list 
+    avg_chng=round(add/length,2)        #Calculating the Average Change using the (total sum/total number) formula and rounding the result to 3 decimal points.
+    Total_months=len(months)            #Finding the Total Months by finding the no of items in the months list.
 
 
-    print("Financial Analysis")
+    print("Financial Analysis")       #Printing all the results as instructed.
 
     print("---------------------------\n")
       
@@ -63,6 +69,7 @@ with open(file_path) as data:
     print(f"Greatest Increase in Profits:{dates[N+1]} "+ "($" + str(grt_value)+ ")\n" )
     
     print(f"Greatest Decrease in Profits:{dates[M+1]}"+ "($" + str(min_value)+ ")\n")
+    
    
     
     
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,21 +9,23 @@ D=0
 R=0
 
 
-
+#Assigning the path of the source data to the paths variable.
 
 paths=os.path.join("Resources","election_data.csv")
+
+#opening the file in read mode.
 with open(paths) as data:
     lines=csv.reader(data,delimiter=",")
-    header=next(lines)
+    header=next(lines)         #storing header in the header variable.
     
 
     for row in lines:
-       ballet.append(row[0])
-       candidate.append(row[2])
+       ballet.append(row[0])      # Store the Ballot_Id column in the ballet list.
+       candidate.append(row[2])   # Store the Candidate column in the candidate list.
    
-    tot_vot=len(ballet)
+    tot_vot=len(ballet)           #Finding the Total Votes by getting no of items in the ballet list.
 
-    for i in range(0,len(candidate)):
+    for i in range(0,len(candidate)):                    #Finding the count of vote for each candidate using If conditional statement.
         if candidate[i]=="Charles Casper Stockham":
             C=C+1
         elif candidate[i]=="Diana DeGette":
@@ -31,17 +33,17 @@ with open(paths) as data:
         elif candidate[i]=="Raymon Anthony Doane":
             R=R+1
 
-    C_perc=round((C/tot_vot)*100,3)
+    C_perc=round((C/tot_vot)*100,3)                 #Calculating the percentage of vote for  each candidate and adding the results to the vote_count dictionary.
     vote_count["Charles Casper Stockham"]=C_perc
     D_perc=round((D/tot_vot)*100,3)
     vote_count["Diana DeGette"]=D_perc
     R_perc=round((R/tot_vot)*100,3)
     vote_count["Raymon Anthony Doane"]=R_perc
 
-    high_count=vote_count.values()
+    high_count=vote_count.values()              #Finding the highest value in the dictionary and using that value we can find the winner.
     Lead=max(high_count)
 
-print("Election Results")
+print("Election Results")                         #Printing the calculated values.
 print("-------------------------\n")
 
 print(f"Total Votes: {tot_vot}\n")
@@ -53,7 +55,7 @@ print(f"Raymon Anthony Doane:{R_perc}% ("+str(R)+")\n")
 print("-------------------------\n")
 
     
-for name,vote in vote_count.items():
+for name,vote in vote_count.items():             #Printing the corresponding Key of the high value in the dictionary.So that we can find the winner.
             if vote==Lead:
                  print(f"Winner: {name}")
 
